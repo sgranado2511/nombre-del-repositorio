f88a6648bdc6c50cbd657fe71750cccc8c408e30
avanza <avanza@10.0.2.15>
2014-05-12 11:26:51 +0100

    version 1.0

---
+++ src/contadornumeros/ContadorNumeros2.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sergio Granado
+ */
+public class ContadorNumeros2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        String numero;
+        int i=0,j=0;
+        
+         while(j!=1)
+        {
+        Scanner entrada = new Scanner (System.in);
+        numero = entrada.nextLine();
+        j=numero.length();
+        
+        if(numero.charAt(0)=='-')
+                {
+                    j--;
+                }
+        System.out.println(numero+"="+j);
+        }
+        
+        
+    }
+}
